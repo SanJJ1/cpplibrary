a1476c1a4ef81c86b67b7024af8be7790b6325cd
Sanjay Janardhan <21janardhansanjay@gmail.com>
2020-08-25 00:20:14 -0400

    adds template

---
+++ template.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <numeric>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        n;
+    }
+}
+
+
